51792888af93c8e3e167edb036e5ef01ca641a37
JaredLMayer <JaredLMayer@gmail.com>
2016-01-28 21:09:28 -0500

    Successful routing.:

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var morgan = require('morgan');
 var swig = require('swig');
 	swig.setDefaults({ cache: false });
 var engines = require('consolidate');
+var routes = require('./routes/');
+
+
+app.use('/', routes);
 app.engine('html', engines.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-
+app.use(express.static('public'))
 
 var locals = {
     title: 'An Example',
@@ -31,14 +35,10 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use('/special', function(req, res){
-	res.send("You've reached the special area");
-});
-
 var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
 app.get('/', function (req, res){
 	res.render('index', {title: 'Hall of Fame', people: people});
 });
 
-app.use('/news', require('./news-router'));
 
